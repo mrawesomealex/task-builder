77aacfc5836b905d9c46f1b171429c52dbf13dc7
Alexander Albov <sasha_albov@ukr.net>
2018-04-24 13:50:10 +0300

    Инициализация проекта

---
+++ task-builder-client/src/router/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Profile from '@/components/Profile'
+import Library from '@/components/Library'
+import Project from '@/components/Project'
+
+//Prject Interfaces
+import Diagrams from '@/components/project/DiagramsInterface'
+import Tasks from '@/components/project/TasksInterface'
+import Users from '@/components/project/UsersInterface'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: HelloWorld
+    },
+    {
+      path: '/profile',
+      name: 'UserProfile',
+      component: Profile,
+      children: [
+        {
+          path: 'projects',
+          name: 'library',
+          component:Library
+        },
+        {
+          path: 'project',
+          name: 'project',
+          component:Project,
+          children: [
+            {
+              path: 'tasks',
+              name: 'tasks',
+              component:Tasks
+            },
+            {
+              path: 'diagrams',
+              name: 'diagrams',
+              component:Diagrams
+            },
+            {
+              path: 'users',
+              name: 'users',
+              component:Users
+            }         
+          ]
+        }        
+      ]
+    }
+  ]
+})
